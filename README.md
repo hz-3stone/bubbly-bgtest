99a35bc0c9809370da56203f5e64168e3a9abbb6
三ツ石皓太郎 <3stone@hertz.ne.jp>
2025-12-10 09:36:55 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ふわふわの泡の背景を作成できるJS「bubbly-bg.js」
+
+## 公式のGithub
+https://github.com/tipsy/bubbly-bg
+
+## 参考記事
+https://blog.8bit.co.jp/?p=18021
---
+++ src/components/bg.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { onMounted } from 'vue';
+
+onMounted(async () => {
+  await import('bubbly-bg');
+  bubbly({
+    blur: 0, //ぼかし
+    colorStart: '#194167', //背景グラデーション始まりの色
+    colorStop: '#112144', //背景グラデーション終わりの色
+    radiusFunc:() => 2 + Math.random() * 10, //大きさ
+    angleFunc:() => -Math.PI / 2, //向き
+    velocityFunc:() => Math.random() * 0.5, //速さ
+    bubbleFunc:() => `hsla(${200 + Math.random() * 50}, 100%, 65%, .1)`, //色彩(hsla)
+    bubbles:20, //個数
+    compose: 'source-over', //色ブレンド
+    canvas: background //キャンバスID指定
+  });
+});
+
+</script>
+
+<template>
+  <canvas id="background" style=""></canvas>
+</template>
+
+<style scoped>
+  #background {
+    position:fixed;
+    z-index:-1;
+    left:0;
+    top:0;
+    min-width:100vw;
+    min-height:100vh;
+  }
+</style>
